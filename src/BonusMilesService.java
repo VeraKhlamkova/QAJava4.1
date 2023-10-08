debb73d9bb4fe6dd719cdf00ecd704c39ad84c16
Vera Khlamkova <sovasov2015@gmail.com>
2023-10-08 21:44:18 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+
+    public int calculate() {
+
+       int price = 15_000;
+       int bonus = 20;
+        int miles = price / bonus;
+
+        return miles;
+    }
+
+
+
+}
